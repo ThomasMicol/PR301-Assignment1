81cf7c7b5ec8f8a06da2961bff324f714727d820
Steven <stevensnelling@hotmail.com>
2017-08-17 09:58:10 +1200

    added to the IGraphView

--- source/View/GraphView.py
+++ source/View/GraphView.py
@@ -1,9 +1,19 @@
 from View.IGraphView import IGraphView
+from plotly import *
+
 
 class GraphView(IGraphView):
 
     def sales_by_gender_graph(self, data_arr):
-        print("child")
+        graph_data = {
+            'data': [{'labels': ['Residential', 'Non-Residential', 'Utility'],
+                      'values': [19, 26, 55],
+                      'type': 'pie'}],
+            'layout': {
+                'title': 'Forcasted 2014 U.S. PV Installations by Market Segment'}
+        }
+
+        self.show_graph(graph_data)
 
     def show(self, show_string):
         print(show_string)
@@ -23,3 +33,7 @@ class GraphView(IGraphView):
         person_data.append(self.read("Enter your birthday, e.g. dd-mm-yyyy: "))
         person_data_arr.append(person_data)
         return person_data_arr
+
+    @staticmethod
+    def show_graph(graph_data):
+        plotly.offline.plot(graph_data)
