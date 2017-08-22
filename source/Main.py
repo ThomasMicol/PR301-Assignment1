a5b855d887df3011049dd2fadd67397b75a3d233
Thomas Micol <thomasmicol@gmail.com>
2017-08-22 23:02:33 +1200

    Started to write doc strings to my code.

--- source/Main.py
+++ source/Main.py
@@ -1,3 +1,11 @@
+"""Written By Thomas
+
+This is the main file. The dependency injection is handled from here
+and the cmdloop is started.
+
+ """
+
+
 from InterpreterController import InterpreterController
 from View.GraphView import *
 from Model.Interpreter import *
@@ -5,7 +13,6 @@ from Model.FileHandler.FileHandler import *
 from Model.DataValidation.DataValidator import *
 from Model.Database.Database import *
 import sys
-
 
 
 if __name__ == '__main__':
--- source/Model/Database/IDatabase.py
+++ source/Model/Database/IDatabase.py
@@ -1,3 +1,14 @@
+"""Written By Thomas
+
+This defines the abstract methods for the Database.
+
+If any of these methods are called without their implementation being defined in
+a child class, the NotImplemented error will be thrown. This results in the system
+closing however.
+
+"""
+
+
 class IDatabase(object):
 
     def create_connection(self, database_name='mydb'):
