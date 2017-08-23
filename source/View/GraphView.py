7476908a9c29acc37d0a5d503a2de100d5ae6354
Steven <stevensnelling@hotmail.com>
2017-08-24 08:24:37 +1200

    Added 2 graphs -bar graph for sales by gender -pie chart for number of employees by gender

--- source/View/GraphView.py
+++ source/View/GraphView.py
@@ -14,12 +14,49 @@ class GraphView(IGraphView):
     #
     #
     def sales_by_gender_graph(self, data_arr):
-        # TODO
-        pass
+        # By Steven Snelling
+        gender_data = []
+        sales_data = []
+        for person in data_arr:
+            gender_data.append(person[1])
+            sales = int(person[3])
+            sales_data.append(sales)
+            print(sales_data)
+            print(gender_data)
+        graph_data = [ob.Bar(
+            x=gender_data,
+            y=sales_data
+        )]
+        graph_format = ob.Layout(
+            title="Gender Verse Sales Bar Graph",
+            xaxis=dict(
+                title="Gender"
+            ),
+            yaxis=dict(
+                title="Sales"
+            )
+        )
+        graph = ob.Figure(data=graph_data, layout=graph_format)
+        self.show_graph(graph)
 
     def employees_by_gender_graph(self, data_arr):
-        # TODO
-        pass
+        # By Steven Snelling
+        male_count = 0
+        female_count = 0
+        for person in data_arr:
+            if person[1] == "M":
+                male_count += 1
+            if person[1] == "F":
+                female_count += 1
+
+        graph_data = {
+            'data': [{'labels': ['Male', 'Female'],
+                      'values': [male_count, female_count],
+                      'type': 'pie'}],
+            'layout': {
+                'title': 'Number of Employees by Gender'}
+        }
+        self.show_graph(graph_data)
 
     def age_verse_salary_graph(self, data_arr):
         # Written By Thomas
