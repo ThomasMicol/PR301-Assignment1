1cb5af4b24ea960c997112a57f049ea69a7d9604
Steven <stevensnelling@hotmail.com>
2017-08-24 09:09:17 +1200

    Added a type error expection

--- source/Model/DataValidation/DataValidator.py
+++ source/Model/DataValidation/DataValidator.py
@@ -10,51 +10,56 @@ class DataValidator(IDataValidator):
         # where valid person data goes
         clean_people = []
 
-        for dirty_person in dirty_data_arr:
-            # print("dirty person", dirty_person)
+        try:
+            for dirty_person in dirty_data_arr:
+                # print("dirty person", dirty_person)
 
-            if len(dirty_person) == 7:
-                cleaned_person = []
-                # print("data is correct length")
+                if len(dirty_person) == 7:
+                    cleaned_person = []
+                    # print("data is correct length")
 
-                if self.validate_empid(str(dirty_person[0])):
-                    print("Valid empid: " + str(dirty_person[0]))
-                    cleaned_person.append(str(dirty_person[0]))
+                    if self.validate_empid(str(dirty_person[0])):
+                        print("Valid empid: " + str(dirty_person[0]))
+                        cleaned_person.append(str(dirty_person[0]))
 
-                if self.validate_gender(str(dirty_person[1])):
-                    print("Valid gender: " + str(dirty_person[1]))
-                    cleaned_person.append(str(dirty_person[1]))
+                    if self.validate_gender(str(dirty_person[1])):
+                        print("Valid gender: " + str(dirty_person[1]))
+                        cleaned_person.append(str(dirty_person[1]))
 
-                if self.validate_age(str(dirty_person[2])):
-                    print("Valid age: " + str(dirty_person[2]))
-                    cleaned_person.append(str(dirty_person[2]))
+                    if self.validate_age(str(dirty_person[2])):
+                        print("Valid age: " + str(dirty_person[2]))
+                        cleaned_person.append(str(dirty_person[2]))
 
-                if self.validate_sales(str(dirty_person[3])):
-                    print("Valid Sales: " + str(dirty_person[3]))
-                    cleaned_person.append(str(dirty_person[3]))
+                    if self.validate_sales(str(dirty_person[3])):
+                        print("Valid Sales: " + str(dirty_person[3]))
+                        cleaned_person.append(str(dirty_person[3]))
 
-                if self.validate_bmi(str(dirty_person[4])):
-                    print("Valid BMI: " + str(dirty_person[4]))
-                    cleaned_person.append(str(dirty_person[4]))
+                    if self.validate_bmi(str(dirty_person[4])):
+                        print("Valid BMI: " + str(dirty_person[4]))
+                        cleaned_person.append(str(dirty_person[4]))
 
-                if self.validate_salary(str(dirty_person[5])):
-                    print("Valid salary: " + str(dirty_person[5]))
-                    cleaned_person.append(str(dirty_person[5]))
+                    if self.validate_salary(str(dirty_person[5])):
+                        print("Valid salary: " + str(dirty_person[5]))
+                        cleaned_person.append(str(dirty_person[5]))
 
-                if self.validate_birthday(str(dirty_person[6])):
-                    print("Valid birthday: " + str(dirty_person[6]))
-                    cleaned_person.append(str(dirty_person[6]))
-            else:
-                return "Not enough feilds" + str(len(dirty_person))
+                    if self.validate_birthday(str(dirty_person[6])):
+                        print("Valid birthday: " + str(dirty_person[6]))
+                        cleaned_person.append(str(dirty_person[6]))
+                else:
+                    return "Not enough feilds" + str(len(dirty_person))
 
-            # print("Cleaned data before filter: ", cleaned_person)
+                # print("Cleaned data before filter: ", cleaned_person)
 
-            filter(None, cleaned_person)
+                filter(None, cleaned_person)
 
-            print("Cleaned person after filter: ", cleaned_person)
+                print("Cleaned person after filter: ", cleaned_person)
 
-            if len(cleaned_person) == 7:
-                clean_people.append(cleaned_person)
+                if len(cleaned_person) == 7:
+                    clean_people.append(cleaned_person)
+
+        except TypeError:
+            print(TypeError)
+            print("You have submited the wrong data type")
 
         print("Cleaned people after filter: ", clean_people)
 
